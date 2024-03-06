f3ce1a077b8884959bcbe167e5123876bb607bb4
arya060203 <aryaalways761@gmail.com>
2024-03-06 12:49:13 +0800

    first commit

---
+++ Arya_Always_2105176060/Visual_Programming_Task1/weekly_task_1/Contoh Tugas/task.py
@@ -0,0 +1,22 @@
+class Mahasiswa:
+    def __init__(self, nama, kelas, prodi, fakultas, tempat_tinggal, asal):
+        self.nama = nama
+        self.kelas = kelas
+        self.prodi = prodi
+        self.fakultas = fakultas
+        self.tempat_tinggal = tempat_tinggal
+        self.asal = asal
+
+    def tampilkan_info(self):
+        print("Nama:", self.nama)
+        print("Kelas:", self.kelas)
+        print("Prodi:", self.prodi)
+        print("Fakultas:", self.fakultas)
+        print("Tempat Tinggal:", self.tempat_tinggal)
+        print("Asal:", self.asal)
+
+# Contoh penggunaan:
+mahasiswa1 = Mahasiswa("Arya Always", "2021 B", "Pendidikan Komputer", "FKIP", "Samarinda", "Penajam")
+mahasiswa1.tampilkan_info()
+
+
